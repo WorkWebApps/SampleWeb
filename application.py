82159f04f13289b58878df635eb29f9c7aeaabc8
Patryk Sowa <PSowa@rockwellautomation.com>
2020-08-06 12:17:25 +0200

    first

--- application.py
+++ application.py
@@ -1,4 +1,12 @@
 from flask import Flask
+from keras.applications import ResNet50
+from keras.preprocessing.image import img_to_array
+from keras.applications import imagenet_utils
+from PIL import Image
+import numpy as np
+import flask
+import io
+
 app = Flask(__name__)
 
 @app.route("/")
